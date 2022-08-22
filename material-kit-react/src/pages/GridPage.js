33425816d644c95d5209b4a083d2e64b2b6bc84e
IN YOUNG <inyoung9235@daum.net>
2022-08-22 16:34:37 +0900

    그리드 옵션 추가

--- material-kit-react/src/pages/GridPage.js
+++ material-kit-react/src/pages/GridPage.js
@@ -3,29 +3,121 @@ import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react'
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
 import Page from 'src/components/Page';
-import { Button, Container, Stack, Typography } from '@mui/material';
+import { Button, ButtonGroup, Container, Stack, Typography } from '@mui/material';
 import Iconify from 'src/components/Iconify';
 import { AgGridReact } from 'ag-grid-react';
 
-// https://www.ag-grid.com/react-data-grid/column-updating-definitions/
+// 컬럼 셋팅
+const getColumnDefs = () => {
+  return [
+    { field: 'athlete', width: 210 },
+    { field: 'age', width: 120, type: 'numberColumn' },
+    { field: 'country', pinned: 'left', width: 140, rowDrag: true },
+    { field: 'sport' },
+    { field: 'year', width: 120 },
+    {
+      field: 'date',
+      width: 160,
+      filter: 'agDateColumnFilter',
+      filterParams: {
+        // provide comparator function
+        comparator: (filterLocalDateAtMidnight, cellValue) => {
+          const dateAsString = cellValue;
 
-function GridPage() {
+          if (dateAsString == null) {
+            return 0;
+          }
+
+          // In the example application, dates are stored as dd/mm/yyyy
+          // We create a Date object for comparison against the filter date
+          const dateParts = dateAsString.split('/');
+          const year = Number(dateParts[2]);
+          const month = Number(dateParts[1]) - 1;
+          const day = Number(dateParts[0]);
+          const cellDate = new Date(year, month, day);
+
+          // Now that both parameters are Date objects, we can compare
+          if (filterLocalDateAtMidnight.getTime() === cellDate.getTime()) {
+            return 0;
+          }
+          if (cellDate < filterLocalDateAtMidnight) {
+            return -1;
+          }
+          if (cellDate > filterLocalDateAtMidnight) {
+            return 1;
+          }
+          return 0;
+        },
+        browserDatePicker: true,
+      },
+    },
+    {
+      headerName: 'Medals',
+      groupId: 'medalsGroup',
+      children: [
+        // using medal column type
+        { headerName: 'Gold', field: 'gold', type: 'medalColumn' },
+        { headerName: 'Silver', field: 'silver', type: 'medalColumn' },
+        { headerName: 'Bronze', field: 'bronze', type: 'medalColumn' },
+        {
+          headerName: 'Total',
+          field: 'total',
+          type: 'medalColumn',
+          columnGroupShow: 'closed',
+          width: 120,
+        },
+      ],
+    },
+    // { field: 'gold', initialWidth: 100 },
+    // { field: 'silver', initialWidth: 100 },
+    // { field: 'bronze', initialWidth: 100 },
+    // { field: 'total', initialWidth: 100 },
+  ];
+};
+
+const GridPage = () => {
   const gridRef = useRef(); // Optional - for accessing Grid's API
   const [rowData, setRowData] = useState(); // Set rowData to Array of Objects, one Object per Row
 
   // 그리드 컬럼 데이터
-  const [columnDefs] = useState([
-    { field: 'make', flex: 2 },
-    { field: 'model', flex: 2 },
-    { field: 'price', flex: 2 },
-    { field: 'total', flex: 1 },
-  ]);
+  const [columnDefs] = useState(getColumnDefs());
 
   // 그리드 옵션
-  const defaultColDef = {
-    resizable: true, // 컬럼 사이즈 정렬
-    sortable: true, // 정렬
-    filter: true, // 필터
+  const defaultColDef = useMemo(() => {
+    return {
+      resizable: true, // 컬럼 사이즈 정렬
+      sortable: true, // 정렬
+      filter: true, // 필터
+      pinned: null,
+      sort: null,
+      // editable: true, // 데이터 작성 가능
+      floatingFilter: true, // 필터 테이블에서 보여주기
+      filterParams: {
+        buttons: ['reset', 'cancel'],
+        closeOnApply: true,
+      },
+    };
+  }, []);
+
+  const columnTypes = useMemo(() => {
+    return {
+      numberColumn: { width: 110, filter: 'agNumberColumnFilter' },
+      medalColumn: { width: 100, columnGroupShow: 'open', filter: 'agNumberColumnFilter' },
+      nonEditableColumn: { editable: false },
+    };
+  }, []);
+
+  const sizeToFit = useCallback(() => {
+    gridRef.current.api.sizeColumnsToFit();
+  }, []);
+
+  // data길이에 맞추어 컬럼 width 설정
+  const autoSizeAll = (skipHeader) => {
+    const allColumnIds = [];
+    gridRef.current.columnApi.getColumns().forEach((column) => {
+      allColumnIds.push(column.getId());
+    });
+    gridRef.current.columnApi.autoSizeColumns(allColumnIds, skipHeader);
   };
 
   // 그리드 클릭 시, 이벤트
@@ -38,8 +130,11 @@ function GridPage() {
     gridRef.current.api.deselectAll(); // 선택 해제
   }, []);
 
-  const buttonStateInit = useCallback(() => {
-    gridRef.current.api.deselectAll(); // 선택 해제
+  // 컬럼 설정한 것 초기화
+  const onBtWithState = useCallback(() => {
+    gridRef.current.api.setColumnDefs(getColumnDefs());
+    autoSizeAll(false);
+    gridRef.current.api.setFilterModel(null); // 필터 초기화
   }, []);
 
   const containerStyle = useMemo(() => ({ width: '100%', height: 500 }), []);
@@ -47,7 +142,7 @@ function GridPage() {
 
   // 서버 통신 -> 데이터 가져옴
   useEffect(() => {
-    fetch('https://www.ag-grid.com/example-assets/row-data.json')
+    fetch('https://www.ag-grid.com/example-assets/olympic-winners.json')
       .then((result) => result.json())
       .then((rowData) => setRowData(rowData));
   }, []);
@@ -59,21 +154,15 @@ function GridPage() {
           <Typography variant="h4" gutterBottom>
             Grid
           </Typography>
-          <Button
-            variant="contained"
-            onClick={buttonListener}
-            startIcon={<Iconify icon="mdi:checkbox-blank-off-outline" />}
-          >
-            deselect
-          </Button>
-
-          <Button
-            variant="contained"
-            onClick={buttonStateInit}
-            startIcon={<Iconify icon="mdi:checkbox-blank-off-outline" />}
-          >
-            buttonInit
-          </Button>
+          <ButtonGroup variant="contained" aria-label="outlined primary button group">
+            <Button onClick={buttonListener} startIcon={<Iconify icon="mdi:checkbox-blank-off-outline" />}>
+              선택 해제
+            </Button>
+            <Button onClick={onBtWithState} startIcon={<Iconify icon="mdi:reload" />}>
+              테이블 설정 초기화
+            </Button>
+            <Button onClick={sizeToFit}>Three</Button>
+          </ButtonGroup>
         </Stack>
         <div style={containerStyle}>
           <div style={gridStyle} className="ag-theme-alpine">
@@ -82,7 +171,10 @@ function GridPage() {
               rowData={rowData} // Row Data for Rows
               columnDefs={columnDefs} // Column Defs for Columns
               defaultColDef={defaultColDef} // Default Column Properties
-              // rowSelection="multiple" // 클릭시, row선택유지
+              columnTypes={columnTypes}
+              rowDragManaged="true"
+              animateRows="true"
+              rowSelection="multiple" // 클릭시, row선택유지
               onCellClicked={cellClickedListener} // 클릭한 row의 데이터 가져옴
             />
           </div>
@@ -90,6 +182,6 @@ function GridPage() {
       </Container>
     </Page>
   );
-}
+};
 
 export default GridPage;
